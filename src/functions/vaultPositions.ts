872817d144f36ec9d2730e0764503033e604fd9f
Tatyana Stemkovski <tanya_stemkovski@hotmail.com>
2024-09-11 23:06:17 -0400

    Add liquidy, amounts and tvl to vault positions

--- src/functions/vaultPositions.ts
+++ src/functions/vaultPositions.ts
@@ -5,6 +5,7 @@ import { SupportedDex } from '../types';
 import { validateVaultData } from './vault';
 import { getIchiVaultContract } from '../contracts';
 import { getTokenDecimals } from './_totalBalances';
+import formatBigInt from '../utils/formatBigInt';
 
 const univ3prices = require('@thanpolas/univ3prices');
 
@@ -16,6 +17,10 @@ export type VaultPositionsInfo = {
     tickUpper: number;
     priceLower: number;
     priceUpper: number;
+    liquidity: string;
+    amountToken0: string;
+    amountToken1: string;
+    positionTvl: number;
   }[];
 };
 
@@ -51,11 +56,20 @@ export async function getVaultPositions(
     const limitLower = await vaultContract.limitLower();
     const limitUpper = await vaultContract.limitUpper();
     const currentTick = await vaultContract.currentTick();
+    const basePosition = await vaultContract.getBasePosition();
+    const limitPosition = await vaultContract.getLimitPosition();
     const priceAtBaseLower = getPriceInDepositToken(isInv, tokenDecimals, baseLower);
     const priceAtBaseUpper = getPriceInDepositToken(isInv, tokenDecimals, baseUpper);
     const priceAtLimitLower = getPriceInDepositToken(isInv, tokenDecimals, limitLower);
     const priceAtLimitUpper = getPriceInDepositToken(isInv, tokenDecimals, limitUpper);
     const currentPrice = getPriceInDepositToken(isInv, tokenDecimals, currentTick);
+    const basePositionTvl = !isInv
+    ? Number(formatBigInt(basePosition.amount0, decimals0)) + Number(formatBigInt(basePosition.amount1, decimals1)) * currentPrice
+    : Number(formatBigInt(basePosition.amount1, decimals1)) + Number(formatBigInt(basePosition.amount0, decimals0)) * currentPrice;
+    const limitPositionTvl = !isInv
+    ? Number(formatBigInt(limitPosition.amount0, decimals0)) + Number(formatBigInt(limitPosition.amount1, decimals1)) * currentPrice
+    : Number(formatBigInt(limitPosition.amount1, decimals1)) + Number(formatBigInt(limitPosition.amount0, decimals0)) * currentPrice;
+
     return {
       currentTick,
       currentPrice,
@@ -65,12 +79,20 @@ export async function getVaultPositions(
           tickUpper: baseUpper,
           priceLower: priceAtBaseLower,
           priceUpper: priceAtBaseUpper,
+          liquidity: basePosition.liquidity.toString(),
+          amountToken0: basePosition.amount0.toString(),
+          amountToken1: basePosition.amount1.toString(),
+          positionTvl: basePositionTvl,
         },
         {
           tickLower: limitLower,
           tickUpper: limitUpper,
           priceLower: priceAtLimitLower,
           priceUpper: priceAtLimitUpper,
+          liquidity: limitPosition.liquidity.toString(),
+          amountToken0: limitPosition.amount0.toString(),
+          amountToken1: limitPosition.amount1.toString(),
+          positionTvl: limitPositionTvl,
         },
       ],
     };
