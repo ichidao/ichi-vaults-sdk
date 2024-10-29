e698fa1ab849f5f581430ac334f94cd6972fd3f2
Vladimir Stemkovski <stemkovs@yahoo.com>
2024-10-29 16:45:30 -0400

    0.0.86

--- src/graphql/constants.ts
+++ src/graphql/constants.ts
@@ -215,7 +215,7 @@ export const graphUrls: Record<SupportedChainId, dexGraph> = {
     [SupportedDex.SparkDexV1]: {
       url: 'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/ichi-org/flare-v1-sparkdex/gn',
       publishedUrl:
-        'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/ichi-org/flare-v2-sparkdex/gn',
+        'https://api.goldsky.com/api/public/project_clynrq1h8gam301xx6vgngo9p/subgraphs/ichi-org/flare-v1-sparkdex/gn',
       supportsCollectFees: true,
     },
   },
